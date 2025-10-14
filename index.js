51c84ec19ebb76e5bb694952aeb5555b0321b475
Bharat Singh <Bharats328@gmail.com>
2025-10-14 22:51:08 +0530

    index.js debugged

--- index.js
+++ index.js
@@ -13,29 +13,29 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const newHotel = {
-  name: "New Hotel 1",
-  category: "Mid-Range",
-  location: "123 Main Street, Frazer Town",
-  rating: 4.0,
-  reviews: [],
-  website: "https://hotel-example.com",
-  phoneNumber: "+1234567890",
-  checkInTime: "2:00 PM",
-  checkOutTime: "12:00 PM",
-  amenities: ["Laundry", "Room Service"],
-  priceRange: "$$$ (31-60)",
-  reservationsNeeded: true,
-  isParkingAvailable: true,
-  isWifiAvailable: true,
-  isPoolAvailable: false,
-  isSpaAvailable: false,
-  isRestaurantAvailable: true,
-  photos: [
-    "https://example.com/hotel-photo1.jpg",
-    "https://example.com/hotel-photo2.jpg",
-  ],
-};
+// const newHotel = {
+//   name: "New Hotel 1",
+//   category: "Mid-Range",
+//   location: "123 Main Street, Frazer Town",
+//   rating: 4.0,
+//   reviews: [],
+//   website: "https://hotel-example.com",
+//   phoneNumber: "+1234567890",
+//   checkInTime: "2:00 PM",
+//   checkOutTime: "12:00 PM",
+//   amenities: ["Laundry", "Room Service"],
+//   priceRange: "$$$ (31-60)",
+//   reservationsNeeded: true,
+//   isParkingAvailable: true,
+//   isWifiAvailable: true,
+//   isPoolAvailable: false,
+//   isSpaAvailable: false,
+//   isRestaurantAvailable: true,
+//   photos: [
+//     "https://example.com/hotel-photo1.jpg",
+//     "https://example.com/hotel-photo2.jpg",
+//   ],
+// };
 
 async function createHotel(newHotel) {
   try {
@@ -47,7 +47,7 @@ async function createHotel(newHotel) {
   }
 }
 
-createHotel(newHotel);
+// createHotel(newHotel);
 
 // LESSON BE: BE4.1_HW2
 
